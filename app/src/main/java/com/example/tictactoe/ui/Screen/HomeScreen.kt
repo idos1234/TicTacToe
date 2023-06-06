58cc3a674bed4ff4733cc4b2946a7201053d2a3e
idos1234 <ido1.suliman@gmail.com>
2023-06-06 16:37:32 +0300

    Adding categories to the menu

--- app/src/main/java/com/example/tictactoe/ui/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/tictactoe/ui/Screen/HomeScreen.kt
@@ -1,12 +1,8 @@
 package com.example.tictactoe.ui.Screen
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,122 +12,64 @@ import com.example.tictactoe.ui.theme.BackGround
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tictactoe.R
 import com.example.tictactoe.ui.theme.Secondery
 import com.example.tictactoe.ui.theme.Shapes
-import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(onTwoPlayersClick: () -> Unit = {}, onSinglePlayerClick: () -> Unit = {}) {
-
-    val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
-
-    Scaffold(
-        scaffoldState= scaffoldState,
-        topBar = {
-        topHomeScreenBar(
-            onClick = {
-                scope.launch {
-                    scaffoldState.drawerState.open()
-                }
-            }
+    Row() {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
+                .background(BackGround)
+                .fillMaxSize()
         )
-    },
-        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
-        drawerContent = {
-            HomeScreenMenu()
-        }
-    ){
-        Row() {
-
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier
-                    .background(BackGround)
-                    .fillMaxSize()
+        {
+            Spacer(modifier = Modifier.weight(2f))
+            Text(
+                text = stringResource(id = R.string.app_name),
+                fontWeight = FontWeight.ExtraBold,
+                fontStyle = FontStyle.Italic,
+                fontSize = 50.sp,
+                color = Color.Black
             )
-            {
-                Spacer(modifier = Modifier.weight(2f))
+            Spacer(modifier = Modifier.weight(1f))
+
+            Button(
+                onClick = onSinglePlayerClick,
+                colors = ButtonDefaults.buttonColors(Secondery),
+                shape = Shapes.large,
+            ) {
                 Text(
-                    text = stringResource(id = R.string.app_name),
+                    text = "Single Player",
                     fontWeight = FontWeight.ExtraBold,
                     fontStyle = FontStyle.Italic,
-                    fontSize = 50.sp,
+                    fontSize = 30.sp,
                     color = Color.Black
                 )
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                Button(
-                    onClick = onSinglePlayerClick,
-                    colors = ButtonDefaults.buttonColors(Secondery),
-                    shape = Shapes.large,
-                ) {
-                    Text(
-                        text = "Single Player",
-                        fontWeight = FontWeight.ExtraBold,
-                        fontStyle = FontStyle.Italic,
-                        fontSize = 30.sp,
-                        color = Color.Black
-                    )
-                }
-
-                Spacer(modifier = Modifier.weight(.5f))
-
-                Button(
-                    onClick = onTwoPlayersClick,
-                    colors = ButtonDefaults.buttonColors(Secondery),
-                    shape = Shapes.large,
-                ) {
-                    Text(
-                        text = "Two Players",
-                        fontWeight = FontWeight.ExtraBold,
-                        fontStyle = FontStyle.Italic,
-                        fontSize = 30.sp,
-                        color = Color.Black
-                    )
-                }
-                Spacer(modifier = Modifier.weight(4f))
             }
+
+            Spacer(modifier = Modifier.weight(.5f))
+
+            Button(
+                onClick = onTwoPlayersClick,
+                colors = ButtonDefaults.buttonColors(Secondery),
+                shape = Shapes.large,
+            ) {
+                Text(
+                    text = "Two Players",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 30.sp,
+                    color = Color.Black
+                )
+            }
+            Spacer(modifier = Modifier.weight(4f))
         }
     }
 }
 
-@Composable()
-fun topHomeScreenBar(onClick: () -> Unit) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .height(45.dp)
-        .background(BackGround), verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            imageVector = Icons.Default.Menu,
-            contentDescription = null,
-            modifier = Modifier
-                .weight(1f)
-                .size(30.dp)
-                .clickable(
-                    onClick = onClick
-                ))
-        Spacer(modifier = Modifier.weight(3f))
-    }
-}
-
-@Composable()
-fun HomeScreenMenu() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxHeight()) {
-        Button(onClick = {}) {
-            Text(text = "Home")
-        }
-        Button(onClick = {}) {
-            Text(text = "Settings")
-        }
-        Button(onClick = {}) {
-            Text(text = "About Us")
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/tictactoe/ui/Screen/TicTacToeApp.kt
+++ app/src/main/java/com/example/tictactoe/ui/Screen/TicTacToeApp.kt
@@ -2,22 +2,75 @@ package com.example.tictactoe.ui.Screen
 
 import android.annotation.SuppressLint
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.tictactoe.ui.theme.BackGround
+import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.concurrent.schedule
 
 enum class GameScreen(@SuppressLint("SupportAnnotationUsage") @StringRes val title: String) {
-    Start(title = "TicTacToe"),
+    Start(title = "Home"),
+    Settings(title = "Settings"),
+    AboutUs(title = "About Us"),
     TwoPlayers(title = "Two players"),
     SinglePlayer("SinglePlayer")
+}
+
+@Composable()
+fun HomeScreenMenu(modifier: Modifier) {
+    Spacer(modifier = Modifier.height(30.dp))
+
+    Column() {
+        Icon(
+            imageVector = Icons.Default.Close,
+            contentDescription = null,
+            modifier = modifier)
+
+        Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Home")
+        }
+        Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Settings")
+        }
+        Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "About Us")
+        }
+    }
+}
+
+@Composable()
+fun topHomeScreenBar(onClick: () -> Unit) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(45.dp)
+        .background(BackGround), verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            imageVector = Icons.Default.Menu,
+            contentDescription = null,
+            modifier = Modifier
+                .weight(1f)
+                .size(30.dp)
+                .clickable(
+                    onClick = onClick
+                ))
+        Spacer(modifier = Modifier.weight(3f))
+    }
 }
 
 @Composable
@@ -28,11 +81,57 @@ fun TicTacToeApp(viewModel: TicTacToeViewModel = TicTacToeViewModel()) {
         backStackEntry?.destination?.route ?: GameScreen.Start.name
     )
 
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
     var timesPlayed by remember {
         mutableStateOf(0)
     }
 
-    Scaffold(topBar = {}) {
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+        when(currentScreen) {
+            GameScreen.Start ->
+                topHomeScreenBar(
+                onClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+            )
+            GameScreen.Settings ->
+                topHomeScreenBar(
+                    onClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }
+                )
+            GameScreen.AboutUs ->
+                topHomeScreenBar(
+                    onClick = {
+                        scope.launch {
+                            scaffoldState.drawerState.open()
+                        }
+                    }
+                )
+        }
+    },
+        drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+        drawerContent = {
+            HomeScreenMenu(
+                modifier = Modifier
+                    .clickable (
+                        onClick = {
+                            scope.launch {
+                                scaffoldState.drawerState.close()
+                            }
+                        }
+                    )
+            )
+        }
+    ) {
         innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
