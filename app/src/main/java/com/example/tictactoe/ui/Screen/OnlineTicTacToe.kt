536044a029c2099b10c5618b075b141ca9ccd7f5
idos1234 <ido1.suliman@gmail.com>
2023-08-13 18:02:26 +0300

    Make rooms for online game

--- app/src/main/java/com/example/tictactoe/ui/Screen/OnlineTicTacToe.kt
+++ app/src/main/java/com/example/tictactoe/ui/Screen/OnlineTicTacToe.kt
@@ -8,6 +8,10 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import com.example.tictactoe.data.Boxes
 import com.example.tictactoe.data.OnlineGameUiState
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
@@ -70,96 +74,92 @@ fun OnlineTicTacToe(player: String, context: Context) {
     }
 
     //get database
-    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-
-    while (times == 1) {
-
+    val firebaseDatabase = FirebaseDatabase.getInstance();
+    val databaseReference = firebaseDatabase.getReference("Games");
         //get Players collection from database
-        db.collection("Games").get()
-            //on success
-            .addOnSuccessListener { queryDocumentSnapshots ->
-                //check if collection is empty
-                if (!queryDocumentSnapshots.isEmpty) {
-                    val list = queryDocumentSnapshots.documents
-                    for (d in list) {
-                        val game: OnlineGameUiState? = d.toObject(OnlineGameUiState::class.java)
-                        if ((game!!.player2 == "") && (game.winner == "")) {
-                            val updatedGame = OnlineGameUiState(
-                                id = game.id,
-                                player1 = game.player1,
-                                player2 = player,
-                                winner = game.winner,
-                                boxes = game.boxes
-                            )
-                            db.collection("Games")
-                                .whereEqualTo("id", game.id)
-                                .get().addOnSuccessListener {
-                                    for (document in it) {
-                                        db.collection("Games")
-                                            .document(document.id)
-                                            .set(updatedGame, SetOptions.merge())
-                                    }
-                                }
-                            currentGame = updatedGame
-                            foundPlayer = true
-                            break
-                        }
-                    }
-                    if (currentGame == OnlineGameUiState()) {
-                        val newGame = OnlineGameUiState(
-                            id = list.size.plus(1),
-                            player1 = player,
-                            player2 = "",
-                            winner = "",
-                            boxes = Boxes()
+    databaseReference.addValueEventListener(object : ValueEventListener {
+        //on success
+        override fun onDataChange(snapshot: DataSnapshot) {
+            println("success to get collection")
+            while (times == 1) {
+                println("start searching for game")
+                Loop@ for (Game in snapshot.children) {
+                    val game = Game.getValue(OnlineGameUiState::class.java)
+                    if ((game!!.player2 == "") && (game.winner == "")) {
+                        println("Found game")
+                        val updatedGame = OnlineGameUiState(
+                            id = game.id,
+                            player1 = game.player1,
+                            player2 = player,
+                            winner = game.winner,
+                            boxes = game.boxes
                         )
-
-                        db.collection("Games").add(newGame)
-                            .addOnSuccessListener {
-                                Toast.makeText(context, "Game started", Toast.LENGTH_SHORT).show()
-                            }
-                        currentGame = newGame
+                        databaseReference.child(game.id.toString()).child("player2").setValue(player)
+                        println("joining game")
+                        currentGame = updatedGame
+                        foundPlayer = true
+                        println("stop searching")
+                        break@Loop
                     }
                 }
+                if (currentGame == OnlineGameUiState()) {
+                    println("didn't find game")
+                    val newGame = OnlineGameUiState(
+                        id = snapshot.children.toList().size.plus(1),
+                        player1 = player,
+                        player2 = "",
+                        winner = "",
+                        boxes = Boxes()
+                    )
+                    println("adding new game")
+                    databaseReference.child(snapshot.children.toList().size.plus(1).toString()).setValue(newGame)
+                    currentGame = newGame
+                }
+                times++
             }
-            //on failure
-            .addOnFailureListener {
-                Toast.makeText(
-                    context,
-                    "Fail to get the data.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        times ++
-    }
-
-    db.collection("Games").get()
-        .addOnSuccessListener { queryDocumentSnapshots ->
-            if (!queryDocumentSnapshots.isEmpty) {
-                val list = queryDocumentSnapshots.documents
-                for (d in list) {
-                    val game: OnlineGameUiState? = d.toObject(OnlineGameUiState::class.java)
-                    if (game!!.id == currentGame.id) {
-                        currentGame = game
+            println("searching for update in game")
+            for (Game in snapshot.children) {
+                val game = Game.getValue(OnlineGameUiState::class.java)
+                if (game!!.id == currentGame.id) {
+                    currentGame = game
+                    if (currentGame.player2 != "") {
+                        foundPlayer = true
                     }
                 }
             }
         }
 
+        //on failure
+        override fun onCancelled(error: DatabaseError) {
+            println("Fail to get collection")
+            Toast.makeText(
+                context,
+                "Fail to get the data.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+    )
+
     foundPlayer = currentGame.player2 != ""
 
     if (!foundPlayer) {
+        println("still waiting for player")
         AlertDialog(
             onDismissRequest = {},
             confirmButton = {},
             dismissButton = {},
             text = {
                 Row {
-                   Text(text = "Waiting for player")
+                    Text(text = "Waiting for player")
+                    CircularProgressIndicator()
                 }
-                CircularProgressIndicator()
             }
         )
+    } else {
+        println("found player")
+        println("game started")
+        Toast.makeText(context, "Game started", Toast.LENGTH_SHORT).show()
     }
     OnlineButtonGrid(player = player)
 }
