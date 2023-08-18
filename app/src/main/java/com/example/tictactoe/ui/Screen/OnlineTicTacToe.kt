8ac77ed9659c57b4fa05305d07203390629363e2
idos1234 <ido1.suliman@gmail.com>
2023-08-18 16:47:17 +0300

    Show winner function in online game

--- app/src/main/java/com/example/tictactoe/ui/Screen/OnlineTicTacToe.kt
+++ app/src/main/java/com/example/tictactoe/ui/Screen/OnlineTicTacToe.kt
@@ -3,62 +3,149 @@ package com.example.tictactoe.ui.Screen
 import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tictactoe.data.Boxes
 import com.example.tictactoe.data.OnlineGameUiState
+import com.example.tictactoe.ui.theme.Primery
+import com.example.tictactoe.ui.theme.Secondery
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.firestore.CollectionReference
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
 
 @Composable
 fun SetBoxOnline(game: OnlineGameUiState, boxNumber: Int, playerTurn: String) {
-    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-    val dbGames: CollectionReference = db.collection("Games")
+    //get database
+    val firebaseDatabase = FirebaseDatabase.getInstance()
+    val databaseReference = firebaseDatabase.getReference("Games")
 
     val boxes: Boxes? = when(boxNumber) {
-        1 -> game.boxes.copy(box1 = playerTurn)
-        2 -> game.boxes.copy(box2 = playerTurn)
-        3 -> game.boxes.copy(box3 = playerTurn)
-        4 -> game.boxes.copy(box4 = playerTurn)
-        5 -> game.boxes.copy(box5 = playerTurn)
-        6 -> game.boxes.copy(box6 = playerTurn)
-        7 -> game.boxes.copy(box7 = playerTurn)
-        8 -> game.boxes.copy(box8 = playerTurn)
-        9 -> game.boxes.copy(box9 = playerTurn)
+        1 -> game.boxes.copy(Box1 = playerTurn)
+        2 -> game.boxes.copy(Box2 = playerTurn)
+        3 -> game.boxes.copy(Box3 = playerTurn)
+        4 -> game.boxes.copy(Box4 = playerTurn)
+        5 -> game.boxes.copy(Box5 = playerTurn)
+        6 -> game.boxes.copy(Box6 = playerTurn)
+        7 -> game.boxes.copy(Box7 = playerTurn)
+        8 -> game.boxes.copy(Box8 = playerTurn)
+        9 -> game.boxes.copy(Box9 = playerTurn)
         else -> null
     }
 
-    val updatedGame = OnlineGameUiState(
-        id = game.id,
-        player1 = game.player1,
-        player2 = game.player2,
-        winner = game.winner,
-        boxes = boxes!!
-    )
-    dbGames
-        .whereEqualTo("id", game.id)
-        .get().addOnSuccessListener {
-            for (document in it) {
-                dbGames.document(document.id).set(updatedGame, SetOptions.merge())
-            }
-        }
+    databaseReference.child(game.id.toString()).child("boxes").setValue(boxes)
+    databaseReference.child(game.id.toString()).child("times").setValue(game.times.plus(1))
+
 }
 
 @Composable
-fun OnlineGameButton(game: OnlineGameUiState, boxNumber: Int, box: String, playerTurn: String) {
+fun changePlayerTurn(game: OnlineGameUiState) {
+    var PlayerTurn = if(game.playerTurn == "X") {
+        "O"
+    } else {
+        "X"
+    }
 
+    //get database
+    val firebaseDatabase = FirebaseDatabase.getInstance()
+    val databaseReference = firebaseDatabase.getReference("Games")
+
+    databaseReference.child(game.id.toString()).child("playerTurn").setValue(PlayerTurn)
+
+}
+
+@Composable
+fun OnlineGameButton(game: OnlineGameUiState, boxNumber: Int, box: String, enabled: Boolean) {
+
+    var setBox by remember {
+        mutableStateOf(false)
+    }
+
+    Card(modifier = Modifier.padding(8.dp),shape = RoundedCornerShape(100.dp), border = BorderStroke(3.dp, color = Secondery)) {
+        TextButton(
+            onClick = { setBox = true },
+            enabled = (box == "") && enabled,
+            modifier = Modifier.background(Primery)
+        ) {
+            Text(
+                text = box,
+                style = TextStyle(
+                    textAlign = TextAlign.Center,
+                    fontSize = 35.sp,
+                    color = Color.Black
+                ),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(50.dp)
+                    .fillMaxHeight()
+            )
+        }
+    }
+
+    if (setBox) {
+        SetBoxOnline(game = game, boxNumber = boxNumber, playerTurn = game.playerTurn)
+        changePlayerTurn(game)
+        setBox = false
+    }
 }
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
-fun OnlineButtonGrid(player: String) {
+fun OnlineButtonGrid(game: OnlineGameUiState, myTurn: String?) {
 
+    val enabled = myTurn == game.playerTurn
+
+    Column {
+        Row {
+            OnlineGameButton(game = game, boxNumber = 1, box = game.boxes.Box1, enabled = enabled)
+            OnlineGameButton(game = game, boxNumber = 2, box = game.boxes.Box2, enabled = enabled)
+            OnlineGameButton(game = game, boxNumber = 3, box = game.boxes.Box3, enabled = enabled)
+        }
+        Row {
+            OnlineGameButton(game = game, boxNumber = 4, box = game.boxes.Box4, enabled = enabled)
+            OnlineGameButton(game = game, boxNumber = 5, box = game.boxes.Box5, enabled = enabled)
+            OnlineGameButton(game = game, boxNumber = 6, box = game.boxes.Box6, enabled = enabled)
+        }
+        Row {
+            OnlineGameButton(game = game, boxNumber = 7, box = game.boxes.Box7, enabled = enabled)
+            OnlineGameButton(game = game, boxNumber = 8, box = game.boxes.Box8, enabled = enabled)
+            OnlineGameButton(game = game, boxNumber = 9, box = game.boxes.Box9, enabled = enabled)
+        }
+    }
+
+    //get database
+    val firebaseDatabase = FirebaseDatabase.getInstance()
+    val databaseReference = firebaseDatabase.getReference("Games")
+
+    if(game.times >= 5) {
+        databaseReference.child(game.id.toString()).child("winner").setValue(CheckOnlineWinner(game.boxes))
+        if(game.winner!!.isNotEmpty()) {
+            if (game.winner == myTurn) {
+                //show winner
+                ShowOnlineWinner(text1 = "You won")
+            } else if (game.winner != myTurn) {
+                //show winner
+                ShowOnlineWinner(text1 = "You lose")
+            }
+        }
+        //show tie
+        else if (game.times == 9){
+            ShowOnlineWinner(text1 = "Tie")
+        }
+
+    }
 }
 
 @Composable
@@ -72,21 +159,23 @@ fun OnlineTicTacToe(player: String, context: Context) {
     var times by remember {
         mutableStateOf(1)
     }
+    var myTurn by remember {
+        mutableStateOf<String?>(null)
+    }
+
 
     //get database
-    val firebaseDatabase = FirebaseDatabase.getInstance();
-    val databaseReference = firebaseDatabase.getReference("Games");
-        //get Players collection from database
+    val firebaseDatabase = FirebaseDatabase.getInstance()
+    val databaseReference = firebaseDatabase.getReference("Games")
+
+    //get Players collection from database
     databaseReference.addValueEventListener(object : ValueEventListener {
         //on success
         override fun onDataChange(snapshot: DataSnapshot) {
-            println("success to get collection")
             while (times == 1) {
-                println("start searching for game")
                 Loop@ for (Game in snapshot.children) {
                     val game = Game.getValue(OnlineGameUiState::class.java)
-                    if ((game!!.player2 == "") && (game.winner == "")) {
-                        println("Found game")
+                    if ((game!!.player2 == "") && (game.winner == null)) {
                         val updatedGame = OnlineGameUiState(
                             id = game.id,
                             player1 = game.player1,
@@ -95,29 +184,26 @@ fun OnlineTicTacToe(player: String, context: Context) {
                             boxes = game.boxes
                         )
                         databaseReference.child(game.id.toString()).child("player2").setValue(player)
-                        println("joining game")
                         currentGame = updatedGame
                         foundPlayer = true
-                        println("stop searching")
+                        myTurn = "O"
                         break@Loop
                     }
                 }
                 if (currentGame == OnlineGameUiState()) {
-                    println("didn't find game")
                     val newGame = OnlineGameUiState(
                         id = snapshot.children.toList().size.plus(1),
                         player1 = player,
                         player2 = "",
-                        winner = "",
+                        winner = null,
                         boxes = Boxes()
                     )
-                    println("adding new game")
                     databaseReference.child(snapshot.children.toList().size.plus(1).toString()).setValue(newGame)
                     currentGame = newGame
+                    myTurn = "X"
                 }
                 times++
             }
-            println("searching for update in game")
             for (Game in snapshot.children) {
                 val game = Game.getValue(OnlineGameUiState::class.java)
                 if (game!!.id == currentGame.id) {
@@ -131,7 +217,6 @@ fun OnlineTicTacToe(player: String, context: Context) {
 
         //on failure
         override fun onCancelled(error: DatabaseError) {
-            println("Fail to get collection")
             Toast.makeText(
                 context,
                 "Fail to get the data.",
@@ -144,7 +229,6 @@ fun OnlineTicTacToe(player: String, context: Context) {
     foundPlayer = currentGame.player2 != ""
 
     if (!foundPlayer) {
-        println("still waiting for player")
         AlertDialog(
             onDismissRequest = {},
             confirmButton = {},
@@ -156,10 +240,13 @@ fun OnlineTicTacToe(player: String, context: Context) {
                 }
             }
         )
-    } else {
-        println("found player")
-        println("game started")
-        Toast.makeText(context, "Game started", Toast.LENGTH_SHORT).show()
     }
-    OnlineButtonGrid(player = player)
+    OnlineButtonGrid(game = currentGame, myTurn = myTurn)
+}
+
+@Composable
+fun ShowOnlineWinner(text1: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = text1, fontSize = 25.sp, fontWeight = FontWeight.Bold)
+    }
 }
